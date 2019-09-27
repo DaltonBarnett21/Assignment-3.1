b3f8244c79119cb1aa499809b5be0b50fde459c4
DaltonBarnett21 <54331578+DaltonBarnett21@users.noreply.github.com>
2019-09-27 18:38:13 -0500

    Initial commit

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("Main Scene 1");
+    }
+
+    public void ExitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Outro");
+    }
+
+   
+}
